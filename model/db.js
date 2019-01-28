639727a6959db978958511b8b6ea38039587cb52
timAnim <timothy_chen@live.com>
2019-01-28 17:54:06 +0800

    数据库 的问题

--- model/db.js
+++ model/db.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose')
 const DB_URL = 'mongodb://localhost:27017/bible'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(DB_URL);
+
+let options ={
+	poolSize:5,
+	useNewUrlParser: true,
+}
+
+mongoose.connect(DB_URL, options);
 
 mongoose.connection.on('connect', function () {    
     console.log('Mongoose connection open to ' + DB_URL);  
